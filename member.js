b3b4fd06927170a56e77b8f1d63cdc3ca55a0b57
Gru V <vmistry26@gmail.com>
2024-05-22 11:52:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var skills = {
+        name: 'John',
+        age: 25,
+        skills: ['html', 'css', 'js'],
+        greet: function() {
+            console.log('Hello');
+        }
+    };
+
+    return skills;
+}
